cbdc046bdb3da63c12d77c7952e8ec888df1b9f5
aloysius <alloystory@gmail.com>
2021-05-03 13:36:18 +0800

    add basic three code

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
     compress: true,
     port: 9000,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Snakes 3D',
