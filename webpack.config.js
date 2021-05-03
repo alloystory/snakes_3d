cb1afb2607a76d56608ae43b97833010bc82ff76
aloysius <alloystory@gmail.com>
2021-05-03 12:51:40 +0800

    add base for development

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Snakes 3D',
+    }),
+  ],
+}
