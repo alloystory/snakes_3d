a11503ff3e80583cb53a0a65099d8da105e9899c
aloysius <alloystory@gmail.com>
2021-05-07 21:00:34 +0800

    refactor some more code in World

--- src/components/World.ts
+++ src/components/World.ts
@@ -1,5 +1,4 @@
-import { Mesh, PerspectiveCamera, Scene, WebGLRenderer } from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { PerspectiveCamera, Scene, WebGLRenderer } from 'three'
 import camera from './camera'
 import floor from './floor'
 import renderer from './renderer'
@@ -10,33 +9,23 @@ export default class World {
   private scene: Scene
   private camera: PerspectiveCamera
   private renderer: WebGLRenderer
-  private snake: Mesh
-  private controls: OrbitControls
 
   constructor(container: HTMLElement) {
-    const { clientWidth: width, clientHeight: height } = container
-
+    const { clientWidth, clientHeight } = container
     this.scene = scene.create()
-    this.camera = camera.create(width, height)
-    this.renderer = renderer.create(width, height)
+    this.camera = camera.create(clientWidth, clientHeight)
+    this.renderer = renderer.create(clientWidth, clientHeight)
 
-    this.snake = snake.create()
-    this.scene.add(this.snake)
-
-    this.scene.add(floor.create())
-
+    // Add renderer.
     container.append(this.renderer.domElement)
 
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    this.controls.autoRotate = true
+    // Add Objects.
+    this.scene.add(...[snake.create(), floor.create()])
   }
 
   render() {
     this.renderer.setAnimationLoop(() => {
       this.renderer.render(this.scene, this.camera)
-      this.controls.update()
-      // this.cube.rotateX(0.005)
-      // this.cube.rotateZ(0.005)
     })
   }
 }
