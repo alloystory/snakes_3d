ab8fb310759134924fd588e4eae4a26efc29989c
aloysius <alloystory@gmail.com>
2021-05-18 21:14:22 +0800

    move the snake every second

--- src/components/Animator.ts
+++ src/components/Animator.ts
@@ -1,9 +1,17 @@
-import { Scene, PerspectiveCamera } from 'three'
+import _ from 'lodash'
+import { Object3D, Scene } from 'three'
 import Snake from './Snake'
+
+const snakeMover = _.throttle((snake: Object3D) => {
+  snake.position.x -= 1
+}, 1000)
 
 function animate(scene: Scene) {
   const snake = scene.getObjectByName(Snake.NAME)
-  snake?.rotateZ(-0.1)
+  if (!snake) {
+    throw new Error('Missing snake')
+  }
+  snakeMover(snake)
 }
 
 export default { animate }
