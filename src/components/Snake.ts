5a9e566140c08e95e28b5ebeeedea99ad3b5bbfd
aloysius <alloystory@gmail.com>
2021-05-18 19:44:59 +0800

    change snake to ensure anchor point

--- src/components/Snake.ts
+++ src/components/Snake.ts
@@ -3,11 +3,18 @@ import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three'
 const NAME = 'snake'
 
 function create() {
-  const snakeGeometry = new BoxGeometry()
+  const width = 2
+  const height = 1
+  const depth = 1
+  const snakeGeometry = new BoxGeometry(width, height, depth)
+  // Translate to ensure that anchor point is on the left face of the cube.
+  snakeGeometry.translate(width / 2, 0, 0)
+
   const snakeMaterial = new MeshBasicMaterial()
 
   const snake = new Mesh(snakeGeometry, snakeMaterial)
-  snake.position.z += 0.5
+  snake.position.x -= width / 2
+  snake.position.z += width / 2
   snake.name = NAME
   return snake
 }
